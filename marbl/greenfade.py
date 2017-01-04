33e9273bd3fb29bb37ba790c9113295c00d7a0ba
Andrew <andrewcphotos@gmail.com>
2017-01-04 17:43:52 -0500

    blahfahsfhsdfbj

--- marbl/greenfade.py
+++ marbl/greenfade.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 from SunFounder_PiPlus import *
 import RPi.GPIO as GPIO
+import time
 
 def marbl():
 
-    global RGB
+	global RGB, RE
 
-    RGB = RGB_LED(port='B')
+	RE = Rotary_Encoder(port='A')
+	RGB = RGB_LED(port='B')
 
-    while True:
+	while True:
 
         RGB.breath(0, 255, 0)
-
+		count = RE.rotary_deal(count, step=2)
+		print(count)
+		time.sleep(.5)
 def destroy():
 	RGB.destroy()
+	GPIO.cleanup()
+	RE.destroy()
 	GPIO.cleanup()
 
 if __name__ == "__main__":
